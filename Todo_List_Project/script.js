9bdc27a4746ec3d77b5d9a493671df6d9b3ba483
Glen Smith <glensmith115@gmail.com>
2022-08-02 12:27:44 +1000

    user has to pass integer for delete

--- Todo_List_Project/script.js
+++ Todo_List_Project/script.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+let input = prompt("what would you like to do?")
+const todos = ["Feed chickens", "Code more"]
+while (input !== "quit" && input !== "q") {
+    if (input === "list") {
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i} - ${todos[i]}`);
+        } 
+    } else if (input === "new") {
+        const newTodo = prompt("Ok, what would you like to add?")
+        todos.push(newTodo);
+        console.log(`${newTodo} added to your list`);
+    } else if (input === "delete") {
+        const rmvTodo = parseInt(prompt("Ok, what would you like to remove? please pick number."));
+        if (!Number.isNaN(rmvTodo)) {
+            const deleted = todos.splice(rmvTodo, 1);
+            console.log(`Ok, deleted ${deleted[0]}`);
+        } else {
+            console.log("Unknown index");
+        }
+    }
+    input = prompt("what would you like to do?")
+}
+console.log("OK QUIT THE APP")
