c398029a23a954705010cc37326e3a0ff6fa3285
Glen Smith <glensmith115@gmail.com>
2022-08-02 12:21:06 +1000

    created list, new and delete

--- Todo_List_Project/script.js
+++ Todo_List_Project/script.js
@@ -1,1 +1,19 @@
-console.log("Hello");
+let input = prompt("what would you like to do?")
+const todos = ["Feed chickens", "Code more"]
+while (input !== "quit" && input !== "q") {
+    if (input === "list") {
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i} - ${todos[i]}`);
+        } 
+    } else if (input === "new") {
+        const newTodo = prompt("Ok, what would you like to add?")
+        todos.push(newTodo);
+        console.log(`${newTodo} added to your list`);
+    } else if (input === "delete") {
+        const rmvTodo = prompt("Ok, what would you like to remove? please pick number.");
+        const deleted = todos.splice(rmvTodo, 1);
+        console.log(`Ok, deleted ${deleted[0]}`);
+    }
+    input = prompt("what would you like to do?")
+}
+console.log("OK QUIT THE APP")
